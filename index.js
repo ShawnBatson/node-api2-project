48a4aeb847e5489fb8854ac1381c0c6080acfbfb
Shadewalker1984 <Shadewalker1984@yahoo.com>
2020-04-16 18:20:17 -0500

    added a single config variable (not sure what else to put) and added support for environment variables via two dev dependencies

--- index.js
+++ index.js
@@ -3,18 +3,20 @@ const db = require("./data/db");
 const expressRouter = require("./data/express-router");
 
 const server = express();
-const port = 3000;
+const port = process.env.PORT || 4000;
 
 server.use(express.json());
 server.use("/posts", expressRouter);
 server.use("./comments", expressRouter);
 
 server.get("/", (req, res) => {
-  res.json({
-    message: "Server started successfully",
-  });
+    res.status(200).json({
+        message:
+            process.env.WELCOME_MESSAGE ||
+            `Welcome! The server has been started!`,
+    });
 });
 
 server.listen(port, () => {
-  console.log(`server runing at ${port}`);
+    console.log(`server runing at ${port}`);
 });
